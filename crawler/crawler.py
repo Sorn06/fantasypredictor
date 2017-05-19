6c99dca65a48fb78221a1dec8742ccdb973a853c
Søren 'Sager' Andersen <soeren_9940@live.dk>
2017-05-19 22:58:18 +0200

    new log

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -23,14 +23,28 @@ class MyHTMLParser(HTMLParser):
     #def handle_endtag(self, tag):
         #print("Encountered an end tag :", tag)
 
+    found_team = False
+    get_score = False
     def handle_data(self, data):
         # print("Encountered some data  :", data)
-        found_team = False
-        for team in teams:
-            if team in data:
-                found_team = True
-        if found_team and len(data) < 100:
-            found_team = False
+        #print(data)
+        set_trace = False
+        
+        result = re.match("^([0-9]+-[0-9]+)+$", data)
+        if self.get_score and len(data.strip()) > 1:
+            print("get score")
+            if '-' in data:
+                print("- in data")
+                print(data.strip())
+                exit()
+            self.get_score = False
+
+            print(data) 
+
+        if self.found_team and len(data) < 100:
+            self.found_team = False
+            self.get_score = True
+
             print("new data")
             print(data)
             data = re.sub('\s+',' ', data)
@@ -41,6 +55,10 @@ class MyHTMLParser(HTMLParser):
                 opponents_home.append(data)
                 print(data)
             #exit() 
+            #set_trace = True
+        if set_trace:
+            import pdb; pdb.set_trace()
+
         if "Regular Season" in data:
             self.regular_season = True
             print(data)
@@ -57,13 +75,51 @@ class MyHTMLParser(HTMLParser):
 
         if self.regular_season:
             i = 1
+def parse_html(html):
+    found_season = False
+    new_week = True
+    found_team = False
+    current_team = ''
+    dict_matches = {}
+    current_week = 0
+    for h in html:
+        if '<tbody>' in h:
+            found_season = True
+
+        if found_season:
+            if '<td>' in h:
+                if new_week:
+                    new_week = False
+                    current_week = re.findall(r'\d+', h)
+            if found_team:
+                if '@' in h or current_team in h:
+                    current_team, away = get_team(h)
+                    print(current_team)
+                    found_team = False
+                    exit()
+
+
+            for team in teams:
+                if '/teams/profile?team=' + team in h:
+                    found_team = True
+                    current_team = team
+
+def get_team(string):
+    for team in teams:
+        if '@' in string:
+            return '@' + team, True
+        if team in string:
+            return team, False
 
 def main():
     parser = MyHTMLParser()
-
+    # http://www.pro-football-reference.com/players/B/BradTo00/gamelog/2006/
+    # THIS RIGHT HERE!!!
     response = urllib.request.urlopen("http://www.nfl.com/player/tombrady/2504211/gamelogs?season=2001")
-    page_source = response.read()
-    #print(page_source)
+    page_source = str(response.read())
+    split = page_source.split()
+    parse_html(split)
+    exit()
     lol = page_source.decode("utf-8") 
     parser.feed(lol)
     print(opponents_out)
